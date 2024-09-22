d7dfd1efec277d1a0fff71080fa40a5f08258bc3
cosimoNigro <cosimonigro2@gmail.com>
2024-09-22 19:05:41 +0200

    polished notebooks

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Hands-on tutorial on gamma-ray astronomy for the 13th IDPASC School
 
-Material for the gamma-ray astronomy tutorial at the 13th IDPASC school.
+Material for the gamma-ray astronomy tutorial at the [13th IDPASC school](https://indico.lip.pt/event/1710/).
+
+## Pre-requisites
+Checking beforehand [the basic usage of Astropy's coordinates](https://learn.astropy.org/tutorials/1-Coordinates-Intro.html) [and the very first part of Gammapy's maps and regions tutorial](https://docs.gammapy.org/dev/tutorials/starting/overview.html) will make easier to go throught these tutorials (it will not take more than 15 mins).
+
 
 ## Downlaod the data for this tutorial
-
-Almost all the data we need for this tutorial are available in the `gammapy-data` repository.   
-Download the repository and set a `GAMMAPY_DATA` evironment variable pointing to this path   
+Almost all the data we need for this tutorial are available in the [`gammapy-data`](https://github.com/gammapy/gammapy-data) repository.   
+Download the repository and set (in your `.bashrc` or `.zshrc` file) a `GAMMAPY_DATA` evironment variable pointing to this path   
 ```shell
 export GAMMAPY_DATA=<path to where you downloaded the gammapy-data repository>
 ```
 
 we will also use some LST's Crab Nebula observations recently released to the public.   
 You can download them from [zenodo](https://zenodo.org/records/11445184).   
-Download the `.zip` file in this reposity and unzip it   
+Download the `.zip` file in this same repository and unzip it   
 
 ```shell
 mkdir crab_lst_data
@@ -20,6 +23,10 @@ tar -xzvf 11445184.zip -C crab_lst_data
 ```
 
 ## Download and install the software for this tutorial
-
 Download `gammapy` version `1.2` following [these instructions](https://docs.gammapy.org/1.2/getting-started/index.html#quickstart-setup).   
 It is recommended to create a conda environment.
+
+## Binder
+You can run these notebooks in MyBinder, remember to download the data there though!
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/cosimoNigro/13_idpasc_school_gamma_hands_on/HEAD)
--- solutions/solution_notebook_2.py
+++ solutions/solution_notebook_2.py
@@ -1,5 +1,5 @@
 # let us perform a spectral measurement using all the data
-datasets = Datasets(datasets=[*datasets_hess, *datasets_magic, *datasets_lst])
+datasets = Datasets(datasets=[*datasets_hess, *datasets_magic, datasets_lst[0]])
 print(datasets)
 # apply the model
 datasets.model = [model]
@@ -11,20 +11,26 @@ print(model.spectral_model)
 
 # quickly compute spectral points for all the instruments
 # - MAGIC
-energy_edges_magic = datasets_magic[0].counts.geom.axes["energy"]
+# -- assign the best fit model to the instrument data set
+datasets_magic.models = [model]
+# -- compute FPs
+energy_edges_magic = datasets_magic[0].counts.geom.axes["energy"].edges
 fpe_magic = FluxPointsEstimator(
     energy_edges=energy_edges_magic, source="CrabNebula", selection_optional="all"
 )
 flux_points_magic = fpe_magic.run(datasets=datasets_magic)
 # - H.E.S.S.
-energy_edges_hess = datasets_hess[0].counts.geom.axes["energy"]
+# -- assign the best fit model to the instrument data set
+datasets_hess.models = [model]
+# -- compute FPs
+energy_edges_hess = datasets_hess[0].counts.geom.axes["energy"].edges
 fpe_hess = FluxPointsEstimator(
     energy_edges=energy_edges_hess, source="CrabNebula", selection_optional="all"
 )
 flux_points_hess = fpe_magic.run(datasets=datasets_hess)
 
 
-# make a plot
+# make the plot
 fig, ax = plt.subplots()
 
 # - references first
@@ -51,7 +57,7 @@ spectral_model.plot(
     ax=ax,
     color="gray",
     ls="-",
-    label="LST",
+    label="joint likelihood fit",
     **plot_kwargs,
 )
 spectral_model.plot_error(
@@ -59,8 +65,13 @@ spectral_model.plot_error(
 )
 
 # - flux points of the instruments
-flux_points_lst.plot(ax=ax, sed_type="e2dnde", color="darkorange", label="LST")
-flux_points_magic.plot(ax=ax, sed_type="e2dnde", color="dimgray", label="MAGIC")
-flux_points_hess.plot(ax=ax, sed_type="e2dnde", color="teal", label="H.E.S.S.")
+flux_points_lst.plot(ax=ax, sed_type="e2dnde", marker=".", color="darkorange", label="LST-1")
+flux_points_magic.plot(ax=ax, sed_type="e2dnde", marker=".", color="crimson", label="MAGIC")
+flux_points_hess.plot(ax=ax, sed_type="e2dnde", marker=".", color="teal", label="H.E.S.S.")
 
+ax.set_xlabel(r"$E\,/\,{\rm GeV}$")
+ax.set_ylabel(
+    r"$E^2 {\rm d}\phi/{\rm d}E\,/\,({\rm TeV}\,{\rm cm}^{-2}\,{\rm s}^{-1})$"
+)
+ax.legend()
 plt.show()
